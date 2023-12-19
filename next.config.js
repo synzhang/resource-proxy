7ea0cbfd9f9fdb7d9521c44bbbc4d6784661679b
Syn Zhang <zeyanzhang.china@gmail.com>
2023-12-19 15:27:44 +0800

    feat: config reverse proxy for gemini

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/gemini',
+        destination: 'https://generativelanguage.googleapis.com',
+      },
+    ]
+  },
+}

82d2e64fa7bce91c1c33b6f7474b31557b7faa7b
Syn Zhang <zeyanzhang.china@gmail.com>
2023-12-19 15:38:48 +0800

    chore: config vercel edge regions for gemini api route

---
+++ src/app/api/gemini/index.ts
@@ -0,0 +1,16 @@
+export const config = {
+  runtime: 'edge', 
+  // Available languages and regions for Google AI Studio and Gemini API
+  // https://ai.google.dev/available_regions
+  // https://vercel.com/docs/concepts/edge-network/regions
+  regions: [
+    'cle1',
+    'iad1',
+    'pdx1',
+    'sfo1',
+    'sin1',
+    'syd1',
+    'hnd1',
+    'kix1',
+  ],
+};

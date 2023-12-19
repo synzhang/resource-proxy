36ad9d6773c620187273484eeaefa24eea08ef0b
Syn Zhang <zeyanzhang.china@gmail.com>
2023-12-19 16:29:55 +0800

    fix: correct gemini api route file name

---
+++ src/app/api/gemini/route.ts
@@ -0,0 +1,15 @@
+export const runtime = 'edge'; // 'nodejs' is the default
+// Available languages and regions for Google AI Studio and Gemini API
+// https://ai.google.dev/available_regions
+// https://vercel.com/docs/concepts/edge-network/regions
+
+export const preferredRegion = [
+  'cle1',
+  'iad1',
+  'pdx1',
+  'sfo1',
+  'sin1',
+  'syd1',
+  'hnd1',
+  'kix1',
+];
